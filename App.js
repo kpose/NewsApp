9067f09d20dc1d450700d8ad312596b92e1925d4
kpose <ganihujude@gmail.com>
2019-04-02 16:17:51 +0100

    configure Redux and Reduvers

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+import { Provider } from 'react-redux';
+
+import Router from './app/config/routes'
+// import store from './app/redux/store';
+
+export default class App extends Component {
+    render() {
+        return (
+            /*<Provider store={store}>*/
+                <Router />
+            // </Provider>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/redux/rootReducer.js
@@ -0,0 +1,8 @@
+import {
+    combineReducers
+} from 'redux';
+
+// Combine all the reducers
+const rootReducer = combineReducers({});
+
+export default rootReducer;
